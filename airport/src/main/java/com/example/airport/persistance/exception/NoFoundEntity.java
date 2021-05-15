c2c17a552a941a24e3ca257c75d70d8c8c98a4e7
MateuszCha <mateusz.charyszyn@gmail.com>
2021-05-15 03:16:43 +0200

    add new Exception

---
+++ airport/src/main/java/com/example/airport/persistance/exception/NoFoundEntity.java
@@ -0,0 +1,7 @@
+package com.example.airport.persistance.exception;
+
+public class NoFoundEntity extends BusinessException {
+    public NoFoundEntity(String message) {
+        super(message);
+    }
+}
