421f26ed326ec8a6560b385c8ffee89f88b3ea63
MateuszCha <mateusz.charyszyn@gmail.com>
2021-05-16 18:09:52 +0200

    adding exception IllegalIndexEntiy when index is not properly. BusinessException it is abstract class to create own exception

---
+++ airport/src/main/java/com/example/airport/persistance/exception/IllegalIndexEntity.java
@@ -0,0 +1,7 @@
+package com.example.airport.persistance.exception;
+
+public class IllegalIndexEntity extends BusinessException{
+    public IllegalIndexEntity(String message) {
+        super(message);
+    }
+}
