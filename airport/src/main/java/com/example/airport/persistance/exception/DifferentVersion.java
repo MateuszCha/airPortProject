64abf4fd87f908092decefc0f04d53c80ad0c4a4
MateuszCha <mateusz.charyszyn@gmail.com>
2021-06-11 02:05:59 +0200

    adding new Exception it used when element in database and element entity have different version

---
+++ airport/src/main/java/com/example/airport/persistance/exception/DifferentVersion.java
@@ -0,0 +1,7 @@
+package com.example.airport.persistance.exception;
+
+public class DifferentVersion extends BusinessException {
+    public DifferentVersion(String message) {
+        super(message);
+    }
+}
